6f962a1a387ee8633dd5cef19e6e7e64824b247e
FrancTG <franctauste2000@gmail.com>
2022-12-28 22:53:36 +0100

    malfuncitions fixes

--- js/app.js
+++ js/app.js
@@ -5,7 +5,28 @@ const imgList = {
     4 : 'img/galeria/goku.png'
 };
 
+var previousImg = 2;
+
 function loadImage(pos) {
-    console.log(imgList[pos]);
     document.getElementById("img-preview").setAttribute("src",imgList[pos]);
+    document.getElementsByClassName("image-item").item(pos-1).className = 'image-item image-item-active';
+    document.getElementsByClassName("image-item").item(previousImg-1).className = 'image-item';
+    previousImg = pos;
+}
+
+function galleryScrollRight() {
+        document.getElementsByClassName("playlists").item(0).scrollTo({
+            top: 0,
+            left: document.getElementsByClassName("playlists").item(0).scrollLeft + 365,
+            behavior: 'smooth'
+          });
+    
+}
+
+function galleryScrollLeft() {
+    document.getElementsByClassName("playlists").item(0).scrollTo({
+        top: 0,
+        left: document.getElementsByClassName("playlists").item(0).scrollLeft - 365,
+        behavior: 'smooth'
+      });
 }
